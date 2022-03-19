e27058780f48e4d0544e75b989b01240821a806d
Mohammad Alkhalifah <60398196+mssalkhalifah@users.noreply.github.com>
2022-03-19 20:33:34 +0300

    Fixed import module

--- js/scenes/game.js
+++ js/scenes/game.js
@@ -1,103 +1,104 @@
-import { ParallaxBackground } from "../entities/background.js";
-import { EntitiesManager } from "../entities/entities.js";
-import { Player } from "../entities/player.js";
-import { Healthbar } from "../ui/healthbar.js";
+/* eslint-disable no-undef */
+import { ParallaxBackground } from '../entities/background.js'
+import { EntitiesManager } from '../entities/entities.js'
+import { Player } from '../entities/player.js'
+import { Healthbar } from '../ui/healthbar.js'
 
 /**
  * The game's main scene.
  * Here is where the main game loop and logic.
  */
 export class Game extends Phaser.Scene {
-  constructor() {
-    super();
-    Phaser.Scene.call(this, { key: "Game" });
+  constructor () {
+    super()
+    Phaser.Scene.call(this, { key: 'Game' })
   }
 
-  create() {
-    this.pauseSound = this.sound.add("pause");
-    this.explosionSound = this.sound.add("explosion");
-    this.backgroundSound_1 = this.sound.add("overworld");
-    this.backgroundSound_2 = this.sound.add("overworld_2");
+  create () {
+    this.pauseSound = this.sound.add('pause')
+    this.explosionSound = this.sound.add('explosion')
+    this.backgroundSound_1 = this.sound.add('overworld')
+    this.backgroundSound_2 = this.sound.add('overworld_2')
 
-    this.parallaxBackground = new ParallaxBackground();
-    this.entitiyManager = new EntitiesManager();
-    this.player = new Player();
-    this.healthbar = new Healthbar();
+    this.parallaxBackground = new ParallaxBackground()
+    this.entitiyManager = new EntitiesManager()
+    this.player = new Player()
+    this.healthbar = new Healthbar()
 
-    this.globalSpeed = 0;
-    this.globalTileSpeed = 1;
+    this.globalSpeed = 0
+    this.globalTileSpeed = 1
 
-    this.parallaxBackground.create(this);
-    this.ground = this.add.tileSprite(400, 600, 800, 192, "ground");
-    this.physics.add.existing(this.ground, true);
+    this.parallaxBackground.create(this)
+    this.ground = this.add.tileSprite(400, 600, 800, 192, 'ground')
+    this.physics.add.existing(this.ground, true)
 
-    this.distance = 0;
-    this.scoreText = this.add.text(690, 30, "Score: 0");
+    this.distance = 0
+    this.scoreText = this.add.text(690, 30, 'Score: 0')
 
-    this.entitiyManager.create(this);
-    this.player.create(this);
-    this.healthbar.create(this);
+    this.entitiyManager.create(this)
+    this.player.create(this)
+    this.healthbar.create(this)
 
     this.time.addEvent({
       delay: 500,
       loop: true,
       callback: () => {
-        this.scoreText.text = `Score: ${++this.distance}`;
-      },
-    });
+        this.scoreText.text = `Score: ${++this.distance}`
+      }
+    })
 
     this.time.addEvent({
       delay: 92000,
       loop: false,
-      callback: () => this.backgroundSound_2.play({ volume: 0.5 }),
-    });
+      callback: () => this.backgroundSound_2.play({ volume: 0.5 })
+    })
     this.backgroundSound_1.play({
-      volume: 0.1,
-    });
+      volume: 0.1
+    })
 
     /** Adds overlap collision between the player and other entities */
     this.entitiyManager.entityNames.forEach((name) => {
-      const entityGroup = this.entitiyManager.entities[name];
+      const entityGroup = this.entitiyManager.entities[name]
       this.physics.world.addOverlap(
         this.player.object,
         entityGroup.group,
         (player, otherEntity) => {
           if (!player.invisibility) {
-            player.invisibility = true;
-            player.hit();
-            this.healthbar.draw(player.health);
-            console.group("Collision Player and " + otherEntity.name);
-            console.log("Collision detected!");
-            console.log("Player health: " + player.health);
-            console.groupEnd("Collision Player and " + otherEntity.name);
+            player.invisibility = true
+            player.hit()
+            this.healthbar.draw(player.health)
+            console.group('Collision Player and ' + otherEntity.name)
+            console.log('Collision detected!')
+            console.log('Player health: ' + player.health)
+            console.groupEnd('Collision Player and ' + otherEntity.name)
           }
         }
-      );
-    });
+      )
+    })
 
-    this.physics.add.collider(this.player.object, this.ground);
+    this.physics.add.collider(this.player.object, this.ground)
 
-    const currentScene = this;
-    const playPauseSound = this.pauseSound;
-    this.input.keyboard.on("keydown", function (event) {
+    const currentScene = this
+    const playPauseSound = this.pauseSound
+    this.input.keyboard.on('keydown', function (event) {
       if (event.keyCode === Phaser.Input.Keyboard.KeyCodes.SPACE) {
-        currentScene.sound.pauseAll();
-        playPauseSound.play();
-        currentScene.scene.launch("Pause", currentScene.sound);
-        currentScene.scene.pause();
+        currentScene.sound.pauseAll()
+        playPauseSound.play()
+        currentScene.scene.launch('Pause', currentScene.sound)
+        currentScene.scene.pause()
       }
-    });
+    })
   }
 
-  update() {
-    this.ground.tilePositionX += this.globalTileSpeed;
+  update () {
+    this.ground.tilePositionX += this.globalTileSpeed
 
-    this.parallaxBackground.update(this);
-    this.player.update(this);
-    this.entitiyManager.update(this);
+    this.parallaxBackground.update(this)
+    this.player.update(this)
+    this.entitiyManager.update(this)
 
-    if (this.globalTileSpeed <= 5 && this.distance % 20 == 0) {
-      this.globalTileSpeed += 0.005;
+    if (this.globalTileSpeed <= 5 && this.distance % 20 === 0) {
+      this.globalTileSpeed += 0.005
     }
   }
 }
--- js/scenes/preload.js
+++ js/scenes/preload.js
@@ -1,134 +1,135 @@
+/* eslint-disable no-undef */
 export class Preload extends Phaser.Scene {
-  constructor() {
-    super();
-    Phaser.Scene.call(this, { key: "Preload" });
+  constructor () {
+    super()
+    Phaser.Scene.call(this, { key: 'Preload' })
   }
 
-  preload() {
-    let progressBox = this.add.graphics();
-    let progressBar = this.add.graphics();
-    let width = this.cameras.main.width;
-    let height = this.cameras.main.height;
-    let loadingText = this.make.text({
+  preload () {
+    const progressBox = this.add.graphics()
+    const progressBar = this.add.graphics()
+    const width = this.cameras.main.width
+    const height = this.cameras.main.height
+    const loadingText = this.make.text({
       x: width / 2,
       y: height / 2 - 50,
-      text: "Loading...",
+      text: 'Loading...',
       style: {
-        font: "20px monospace",
-        fill: "#000000",
-      },
-    });
-    let precentText = this.make.text({
+        font: '20px monospace',
+        fill: '#000000'
+      }
+    })
+    const precentText = this.make.text({
       x: width / 2,
       y: height / 2 - 5,
-      text: "0%",
+      text: '0%',
       style: {
-        font: "18px monospace",
-        fill: "0x000000",
-      },
-    });
-    let assetText = this.make.text({
+        font: '18px monospace',
+        fill: '0x000000'
+      }
+    })
+    const assetText = this.make.text({
       x: width / 2,
       y: height / 2 + 50,
-      text: "",
+      text: '',
       style: {
-        font: "18px monospace",
-        fill: "0x000000",
-      },
-    });
-    assetText.setOrigin(0.5, 0.5);
-    precentText.setOrigin(0.5, 0.5);
-    loadingText.setOrigin(0.5, 0.5);
-    progressBox.fillStyle(0x222222, 0.8);
-    progressBox.fillRect(240, 270, 320, 50);
-    console.log("Loading...");
-    this.load.on("progress", (value) => {
-      console.log(value);
-      progressBar.clear();
-      progressBar.fillStyle(0xffffff, 1);
-      progressBar.fillRect(250, 280, 300 * value, 30);
-      precentText.setText(parseInt(value * 100) + "%");
-    });
-    this.load.on("fileprogress", (file) => {
-      console.log(file.src);
-      assetText.setText("Loading assets: " + file.src);
-    });
-    this.load.on("complete", () => {
-      console.log("complete");
-      progressBar.destroy();
-      progressBox.destroy();
-      loadingText.destroy();
-      precentText.destroy();
-      assetText.destroy();
-      this.scene.start("Game");
-      this.scene.stop();
-    });
+        font: '18px monospace',
+        fill: '0x000000'
+      }
+    })
+    assetText.setOrigin(0.5, 0.5)
+    precentText.setOrigin(0.5, 0.5)
+    loadingText.setOrigin(0.5, 0.5)
+    progressBox.fillStyle(0x222222, 0.8)
+    progressBox.fillRect(240, 270, 320, 50)
+    console.log('Loading...')
+    this.load.on('progress', (value) => {
+      console.log(value)
+      progressBar.clear()
+      progressBar.fillStyle(0xffffff, 1)
+      progressBar.fillRect(250, 280, 300 * value, 30)
+      precentText.setText(parseInt(value * 100) + '%')
+    })
+    this.load.on('fileprogress', (file) => {
+      console.log(file.src)
+      assetText.setText('Loading assets: ' + file.src)
+    })
+    this.load.on('complete', () => {
+      console.log('complete')
+      progressBar.destroy()
+      progressBox.destroy()
+      loadingText.destroy()
+      precentText.destroy()
+      assetText.destroy()
+      this.scene.start('Game')
+      this.scene.stop()
+    })
 
-    this.load.image("ground", "assets/background.png");
-    this.load.image("sky", "assets/background_sky.png");
-    this.load.image("cloud", "assets/background_cloud.png");
-    this.load.image("cloud_2", "assets/background_cloud2.png");
-    this.load.image("mountain", "assets/background_mountain.png");
-    this.load.image("mountain", "assets/background_mountain.png");
-    this.load.image("leaderboard", "assets/leaderboard.png");
-    this.load.image("pause", "assets/pause.png");
-    this.load.image("block", "assets/block.png");
-    this.load.image("blockLarge", "assets/block_large.png");
-    this.load.image("spike", "assets/spike.png");
+    this.load.image('ground', 'assets/background.png')
+    this.load.image('sky', 'assets/background_sky.png')
+    this.load.image('cloud', 'assets/background_cloud.png')
+    this.load.image('cloud_2', 'assets/background_cloud2.png')
+    this.load.image('mountain', 'assets/background_mountain.png')
+    this.load.image('mountain', 'assets/background_mountain.png')
+    this.load.image('leaderboard', 'assets/leaderboard.png')
+    this.load.image('pause', 'assets/pause.png')
+    this.load.image('block', 'assets/block.png')
+    this.load.image('blockLarge', 'assets/block_large.png')
+    this.load.image('spike', 'assets/spike.png')
 
-    this.load.audio("lostLife", "assets/audio/lost_life.wav");
-    this.load.audio("overworld", "assets/audio/overworld.mp3");
-    this.load.audio("overworld_2", "assets/audio/overworld_2.mp3");
-    this.load.audio("hit", "assets/audio/hit.wav");
-    this.load.audio("jump", "assets/audio/jump.wav");
-    this.load.audio("pause", "assets/audio/pause.wav");
-    this.load.audio("meteor", "assets/audio/meteor.wav");
-    this.load.audio("explosion", "assets/audio/explosion.wav");
-    this.load.audio("meteorLarge", "assets/audio/meteor_large.wav");
+    this.load.audio('lostLife', 'assets/audio/lost_life.wav')
+    this.load.audio('overworld', 'assets/audio/overworld.mp3')
+    this.load.audio('overworld_2', 'assets/audio/overworld_2.mp3')
+    this.load.audio('hit', 'assets/audio/hit.wav')
+    this.load.audio('jump', 'assets/audio/jump.wav')
+    this.load.audio('pause', 'assets/audio/pause.wav')
+    this.load.audio('meteor', 'assets/audio/meteor.wav')
+    this.load.audio('explosion', 'assets/audio/explosion.wav')
+    this.load.audio('meteorLarge', 'assets/audio/meteor_large.wav')
 
-    this.load.spritesheet("loading", "assets/loading_spinner.png", {
+    this.load.spritesheet('loading', 'assets/loading_spinner.png', {
       frameWidth: 200,
-      frameHeight: 200,
-    });
-    this.load.spritesheet("spacebar", "assets/SPACEALTERNATIVE.png", {
+      frameHeight: 200
+    })
+    this.load.spritesheet('spacebar', 'assets/SPACEALTERNATIVE.png', {
       frameWidth: 98,
-      frameHeight: 21,
-    });
-    this.load.spritesheet("arrow_up", "assets/ARROWUP.png", {
+      frameHeight: 21
+    })
+    this.load.spritesheet('arrow_up', 'assets/ARROWUP.png', {
       frameWidth: 19,
-      frameHeight: 21,
-    });
-    this.load.spritesheet("arrow_down", "assets/ARROWDOWN.png", {
+      frameHeight: 21
+    })
+    this.load.spritesheet('arrow_down', 'assets/ARROWDOWN.png', {
       frameWidth: 19,
-      frameHeight: 21,
-    });
+      frameHeight: 21
+    })
     this.load.spritesheet(
-      "metero_explosion",
-      "assets/meteor_explosion_sheet.png",
+      'metero_explosion',
+      'assets/meteor_explosion_sheet.png',
       {
         frameWidth: 64,
-        frameHeight: 64,
+        frameHeight: 64
       }
-    );
-    this.load.spritesheet("meteor", "assets/meteor_sheet.png", {
+    )
+    this.load.spritesheet('meteor', 'assets/meteor_sheet.png', {
       frameWidth: 16,
-      frameHeight: 32,
-    });
+      frameHeight: 32
+    })
     this.load.image(
-      "mountain_foreground",
-      "assets/background_mountain_foreground.png"
-    );
-    this.load.image("healthbarBorder", "assets/healthbar_border.png");
-    this.load.image("healthbar", "assets/healthbar.png");
-    this.load.spritesheet("bird", "assets/Bird.png", {
+      'mountain_foreground',
+      'assets/background_mountain_foreground.png'
+    )
+    this.load.image('healthbarBorder', 'assets/healthbar_border.png')
+    this.load.image('healthbar', 'assets/healthbar.png')
+    this.load.spritesheet('bird', 'assets/Bird.png', {
       frameWidth: 50,
-      frameHeight: 50,
-    });
-    this.load.spritesheet("player", "assets/player_spritesheet.png", {
+      frameHeight: 50
+    })
+    this.load.spritesheet('player', 'assets/player_spritesheet.png', {
       frameWidth: 52,
-      frameHeight: 58,
-    });
+      frameHeight: 58
+    })
   }
 
-  create() {}
+  create () {}
 }
